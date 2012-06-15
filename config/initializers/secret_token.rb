eadd50191f67638f9b3c8d854d27412515bb92db
Mark Curtiss <MarkCurtiss@gmail.com>
2012-06-14 20:16:08 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EsppCalcSite::Application.config.secret_token = 'a7e46cef62644ac517ef5d8c4f711dfef7bbbbe3901818c45e99bb5a581d597ddd0d4cec396e8678680e11c55b081da11da7d1074fa5a5c5a04813ffa0462eaf'
